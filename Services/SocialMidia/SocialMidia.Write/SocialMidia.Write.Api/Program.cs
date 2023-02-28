58dde3aae20fa7e0a6c55a2dda5941df8b74fb63
Dionis Mendanha <dionismda@gmail.com>
2023-02-28 19:13:36 -0300

    Adicionando estrutura de pasta dos projetos

---
+++ Services/SocialMidia/SocialMidia.Write/SocialMidia.Write.Api/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace SocialMidia.Write.Api;
+
+public static class Program
+{
+    public static int Main(string[] args)
+    {
+        try
+        {
+            CreateHostBuilder(args).Build().Run();
+            return 0;
+        }
+        catch (Exception ex)
+        {
+            return 1;
+        }
+        finally
+        {
+        }
+    }
+
+    private static IHostBuilder CreateHostBuilder(string[] args) =>
+        Host.CreateDefaultBuilder(args)
+            .ConfigureWebHostDefaults(webBuilder => { webBuilder.UseStartup<Startup>(); })
+        ;
+}
---
+++ Services/SocialMidia/SocialMidia.Write/SocialMidia.Write.Api/Startup.cs
@@ -0,0 +1,34 @@
+﻿namespace SocialMidia.Write.Api;
+
+public class Startup
+{
+    private IConfiguration Configuration { get; }
+
+    public Startup(IConfiguration configuration)
+    {
+        Configuration = configuration;
+    }
+
+    public void ConfigureServices(IServiceCollection services)
+    {
+
+    }
+
+    public void Configure(IApplicationBuilder app,IWebHostEnvironment env)
+    {
+        if (env.IsDevelopment())
+            app.UseDeveloperExceptionPage();
+
+
+        app.UseRouting();
+        app.UseHttpsRedirection();
+
+        app.UseAuthentication();
+        app.UseAuthorization();
+
+        app.UseEndpoints(endpoints =>
+        {
+            endpoints.MapDefaultControllerRoute();
+        });
+    }
+}
