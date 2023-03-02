09a4579fb77b90ca906dee5833b5ec331248ec35
Dionis Mendanha <dionismda@gmail.com>
2023-03-02 20:12:52 -0300

    Adicionado mongo-express no docker-compose

--- Services/SocialMidia/SocialMidia.Read/SocialMidia.Read.Infrastructure/GlobalUsings.cs
+++ Services/SocialMidia/SocialMidia.Read/SocialMidia.Read.Infrastructure/GlobalUsings.cs
@@ -1,1 +1,6 @@
 ﻿global using Microsoft.Extensions.DependencyInjection;
+global using SocialMidia.Read.Domain.Interfaces;
+global using _Shared.Domain.Interfaces;
+global using Microsoft.Extensions.Options;
+global using MongoDB.Driver;
+global using SocialMidia.Read.Infrastructure.Configurations;
---
+++ Services/SocialMidia/SocialMidia.Read/SocialMidia.Read.Infrastructure/Repositories/EventStoreRepository.cs
@@ -0,0 +1,29 @@
+﻿namespace SocialMidia.Read.Infrastructure.Repositories;
+
+public class EventStoreRepository : IEventStoreRepository
+{
+    private readonly IMongoCollection<IEventModel> _eventStoreCollection;
+
+    public EventStoreRepository(IOptions<MongoDbConfiguration> config)
+    {
+        var mongoClient = new MongoClient(config.Value.ConnectionString);
+        var mongoDatabase = mongoClient.GetDatabase(config.Value.Database);
+
+        _eventStoreCollection = mongoDatabase.GetCollection<IEventModel>(config.Value.Collection);
+    }
+
+    public async Task<List<IEventModel>> FindAllAsync()
+    {
+        return await _eventStoreCollection.Find(_ => true).ToListAsync().ConfigureAwait(false);
+    }
+
+    public async Task<List<IEventModel>> FindByAggregateId(Guid aggregateId)
+    {
+        return await _eventStoreCollection.Find(x => x.AggregateIdentifier == aggregateId).ToListAsync().ConfigureAwait(false);
+    }
+
+    public async Task SaveAsync(IEventModel @event)
+    {
+        await _eventStoreCollection.InsertOneAsync(@event).ConfigureAwait(false);
+    }
+}
